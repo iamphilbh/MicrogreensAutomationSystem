b17cee2b080d09e89aef91a73705305d57f09db7
iamphilbh <iamphilbh@gmail.com>
2024-03-29 17:21:13 -0400

    switch on/off working

--- RaspberryPi/controler.py
+++ RaspberryPi/controler.py
@@ -5,19 +5,23 @@ GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
 
-def on_connect(client, userdata, flags, rc):
+def on_connect(client:mqtt.Client, userdata, flags, rc:int) -> None:
     print("Connected with result code "+str(rc))
-    client.subscribe("light")
+    client.subscribe("light/switch")
 
-def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+def on_message(client:mqtt.Client, userdata, msg:mqtt.MQTTMessage) -> None:
+    print("Topic: "+msg.topic+", payload: "+str(msg.payload))
 
-    if msg.payload.upper() == "ON":
+    payload = msg.payload.decode('utf-8')  # Decode the payload
+
+    if payload.upper() == "ON":
         print("Turning LED on...")
-        GPIO.output(8, GPIO.LOW)
-    elif msg.payload.upper() == "OFF":    
-        print("Turning LED off...")
         GPIO.output(8, GPIO.HIGH)
+        client.publish("light/status", "ON")
+    elif payload.upper() == "OFF":    
+        print("Turning LED off...")
+        GPIO.output(8, GPIO.LOW)
+        client.publish("light/status", "OFF")
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -26,17 +30,3 @@ client.on_message = on_message
 client.connect("localhost", 1883, 60)
 
 client.loop_forever()
-
-
-# import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-# from time import sleep # Import the sleep function from the time module
-# GPIO.setwarnings(False) # Ignore warning for now
-# GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-# GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW) # Set pin 8 to be an output pin and set initial value to low (off)
-# while True: # Run forever
-#  print("ON")
-#  GPIO.output(8, GPIO.HIGH) # Turn on
-#  sleep(5) # Sleep for 1 second
-#  print("OFF")
-#  GPIO.output(8, GPIO.LOW) # Turn off
-#  sleep(1) # Sleep for 1 second
