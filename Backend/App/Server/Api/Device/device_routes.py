f09b1d24e31c319ce94ebc3462f883836abf0717
Philippe Barrette <pbarrette@cae.com>
2024-04-11 12:03:20 -0400

    renamed and added client file

--- Backend/App/Server/Api/Device/device_routes.py
+++ Backend/App/Server/Api/Device/device_routes.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
-from paho.mqtt.client import Client as MQTTClient
+from gmqtt import Client as MQTTClient, Subscription
 from pydantic import BaseModel
 
+from app.mqtt.client import publish, subscribe
+
 app = FastAPI()
-
-# Create an MQTT client
-client = MQTTClient()
-
-# Connect to the MQTT broker
-client.connect("192.168.0.68", 1883, 60)  # Replace with your broker's hostname/IP and port
 
 class LightState(BaseModel):
     state: str
@@ -20,11 +16,11 @@ async def read_root() -> dict:
 @app.post("/change_light_state")
 async def change_light_state(light_state: LightState) -> dict:
     # Publish a message to the "light/switch" topic
-    client.publish("light/switch", light_state.state)
+    publish("light/switch", light_state.state)
     return {"light_state": light_state.state, "status": "sent"}
 
 @app.get("/light_state")
 async def get_light_state() -> dict:
     # Subscribe to the "light/state" topic
-    result = client.subscribe("light/state")
+    result = subscribe("light/state")
     return {"state": result.get("state", "unknown")}
